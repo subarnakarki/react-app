a2b73486413e9a116cc92a9e5f8d43eae1a6999d
subarna karki <smukhiyabe@gannett.com>
2019-12-04 18:11:51 -0500

    add state to card component and create a like function to update state

--- src/card.js
+++ src/card.js
@@ -1,23 +1,34 @@
 import React, {Component} from 'react';
 
 class Card extends Component {
-    render() {
-        const {image, name, profession, summary} = this.props;
-        return (
-          <div className="column">
-            <div className="card">
-              <img src={image} alt="Mike" style={{width: '100%'}}></img>
-              <div className="container">
-                <h2>{name}</h2>
-                <p className="title">{profession}</p>
-                <p>{summary}</p>
-                <p>{name.toLowerCase()}@email.vccs.edu</p>
-                <p><button className="button">Contact</button></p>
-              </div>
+  constructor() {
+    super();
+    this.state = {
+      likes: 0
+    }
+  }
+
+  addLike(){
+    this.setState({likes: this.state.likes + 1});
+  }
+  render() {
+      const {image, name, profession, summary} = this.props;
+      return (
+        <div className="column">
+          <div className="card">
+            <img src={image} alt="Mike" style={{width: '100%'}}></img>
+            <div className="container">
+              <h2>{name}</h2>
+              <p className="title">{profession}</p>
+              <p>{summary}</p>
+              <p>{name.toLowerCase()}@email.vccs.edu</p>
+              <p>Likes: {this.state.likes}</p>
+              <p><button onClick={this.addLike.bind(this)} className="button">Like</button></p>
             </div>
           </div>
-        )
-    }
+        </div>
+      )
+  }
 }
 
 export default Card;
